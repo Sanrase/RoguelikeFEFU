2f22ff93b2fb3e9e5fbcfdf97a4ec7abce43cdc4
Sanrase <klimenko.aro@students.dvfu.ru>
2023-03-18 10:34:48 +0300

    Add project files.

---
+++ RoguelikeFEFU/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rogue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
