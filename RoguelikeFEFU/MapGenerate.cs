f3fa208805671a825ac785e98588251231d5843c
Sanrase <klimenko.aro@students.dvfu.ru>
2023-03-21 14:14:49 +0300

    Add MapGenerator

---
+++ RoguelikeFEFU/MapGenerate.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace RoguelikeFEFU
+{
+    public class Edge
+    {
+        public int X;
+        public int Y;
+        public bool Horizontal;
+    }
+    internal class MapGenerate
+    {
+        public int width = 100;
+        public int height = 50;
+        public int minRoomSize = 5; 
+        public int maxRoomSize = 10; 
+        public int maxRooms = 12; 
+        public List<Rectangle> rooms = new List<Rectangle>(); 
+        public int[,] map;
+
+        public void GenerateMap()
+        {
+            Random rand = new Random();
+            map = new int[width, height];
+            rooms = new List<Rectangle>();
+            for (int i = 0; i < maxRooms; i++)
+            {
+                int roomWidth = rand.Next(minRoomSize, maxRoomSize + 1);
+                int roomHeight = rand.Next(minRoomSize, maxRoomSize + 1);
+                int roomX = rand.Next(1, width - roomWidth - 1); 
+                int roomY = rand.Next(1, height - roomHeight - 1);
+
+                Rectangle newRoom = new Rectangle(roomX, roomY, roomWidth, roomHeight);
+                bool roomIntersects = false;
+
+                foreach (Rectangle room in rooms)
+                {
+                    if (newRoom.IntersectsWith(room))
+                    {
+                        roomIntersects = true;
+                        break;
+                    }
+                }
+                if (!roomIntersects)
+                {
+                    rooms.Add(newRoom);
+                    for (int x = newRoom.Left; x < newRoom.Right; x++)
+                    {
+                        for (int y = newRoom.Top; y < newRoom.Bottom; y++)
+                        {
+                            if (x > newRoom.Left && x < newRoom.Right && y > newRoom.Top && newRoom.Bottom < y)
+                            {
+                                map[x, y] = 0;
+                            }
+                            else if (x == newRoom.Left || x == newRoom.Right - 1 || y == newRoom.Top || y == newRoom.Bottom - 1)
+                            {
+                                map[x, y] = 1;
+                            }
+                            else
+                            {
+                                map[x, y] = 2;
+                            }
+                        }
+                    }
+                }
+
+            }
+            for (int i = 0; i < rooms.Count - 1; i++)
+            {
+                int firstRoomCenterX = (int)(rooms[i].Left + rooms[i].Width / 2);
+                int firstRoomCenterY = (int)(rooms[i].Top + rooms[i].Height / 2);
+                int secondRoomCenterX = (int)(rooms[i + 1].Left + rooms[i + 1].Width / 2);
+                int secondRoomCenterY = (int)(rooms[i + 1].Top + rooms[i + 1].Height / 2);
+                if (new Random().Next(0, 2) == 0) 
+                {
+                    for (int x = Math.Min(firstRoomCenterX, secondRoomCenterX); x <= Math.Max(firstRoomCenterX, secondRoomCenterX); x++)
+                    {
+                        map[x, firstRoomCenterY] = 3;
+                    }
+                    for (int y = Math.Min(firstRoomCenterY, secondRoomCenterY); y <= Math.Max(firstRoomCenterY, secondRoomCenterY); y++)
+                    {
+                        map[secondRoomCenterX, y] = 3;
+                    }
+                }
+                else
+                {
+                    for (int y = Math.Min(firstRoomCenterY, secondRoomCenterY); y <= Math.Max(firstRoomCenterY, secondRoomCenterY); y++)
+                    {
+                        map[firstRoomCenterX, y] = 3;
+                    }
+                    for (int x = Math.Min(firstRoomCenterX, secondRoomCenterX); x <= Math.Max(firstRoomCenterX, secondRoomCenterX); x++)
+                    {
+                        map[x, secondRoomCenterY] = 3;
+                    }
+                }
+            }
+        }
+
+        public void PrintDungeon()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (map[x, y] == 1)
+                    {
+                        Console.Write('#');
+                    }
+                    else if (map[x, y] == 0)
+                    {
+                        Console.Write(' ');
+                    }
+                    else if (map[x, y] == 2)
+                    {
+                        Console.Write('.');
+                    }
+                    else {
+                        Console.Write('*');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+
+}
--- RoguelikeFEFU/Program.cs
+++ RoguelikeFEFU/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Rogue
+namespace RoguelikeFEFU
 {
-    public class Program
+    internal class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            MapGenerate map = new MapGenerate();
+            map.GenerateMap();
+            map.PrintDungeon();
         }
     }
 }
