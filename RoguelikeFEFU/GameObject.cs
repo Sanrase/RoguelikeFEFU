6c799b4a3735b8e4b3452fd7676c172e459fc8a2
Sanrase <klimenko.aro@students.dvfu.ru>
2023-03-23 16:02:13 +0300

    Add colorPlayer and colorEnemy, class Kobolt, class Snake

--- RoguelikeFEFU/GameObject.cs
+++ RoguelikeFEFU/GameObject.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,11 +13,13 @@ namespace RoguelikeFEFU
     {
         public int X { get; set; }
         public int Y { get; set; }
+        public ConsoleColor Color { get; set; }
 
-        public GameObject(int x, int y)
+        public GameObject(int x, int y, ConsoleColor color)
         {
             this.X = x;
             this.Y = y;
+            Color = color;
         }
     }
 
@@ -25,7 +29,7 @@ namespace RoguelikeFEFU
         public int Attack  { get; set; }
         public int Defense  { get; set; }
 
-        public Entity(int x, int y, ConsoleColor color = ConsoleColor.White) : base(x, y)
+        public Entity(int x, int y, ConsoleColor color = ConsoleColor.White) : base(x, y, color)
         {
             this.Health = 10;
             this.Attack = 1;
@@ -37,7 +41,7 @@ namespace RoguelikeFEFU
         public int Potion { get; set; }
         protected int[] inventory = new int[5];
         public char Symbol { get; set; }
-        public Person(int x, int y, ConsoleColor color) : base(x, y)
+        public Person(int x, int y, ConsoleColor color) : base(x, y, color)
         {
             this.Symbol = '@';
             this.Potion = 3;
@@ -56,13 +60,9 @@ namespace RoguelikeFEFU
             }
         }
 
-        public void Move()
-        {
-        }
-
         public void Attack()
         {
-
+            
         }
 
     }
@@ -70,12 +70,40 @@ namespace RoguelikeFEFU
     internal class Enemy : Entity
     {
         public char Symbol { get; set; }
-        public Enemy(int x, int y, ConsoleColor color) : base(x, y)
+        public int MinCoin { get; set; }
+        public int MaxCoin { get; set; }
+
+        public Enemy(int x, int y, ConsoleColor color) : base(x, y, color)
         {
-            this.Health = 13;
-            this.Attack = 3;
-            this.Defense = 0;
-            this.Symbol = 'S';
+        }
+
+    }
+
+    internal class Kobolt : Enemy
+    {
+        
+        public Kobolt(int x, int y, ConsoleColor color) : base (x, y, color)
+        {
+            Symbol = 'K';
+            Health = 7;
+            Attack = 1;
+            Defense = 1;
+            MaxCoin = 7;
+            MinCoin = 3;
+            
+        }
+    }
+
+    internal class Snake : Enemy
+    {
+        public Snake(int x, int y, ConsoleColor color) : base(x, y, color)
+        {
+            Symbol = 'S';
+            Health = 10;
+            Attack = 2;
+            Defense = 2;
+            MaxCoin = 5;
+            MinCoin = 1;
         }
     }
 
